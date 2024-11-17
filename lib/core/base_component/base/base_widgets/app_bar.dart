71f79718ed08ed69483fdaec00c20b9644ca0759
msiprime <msisakib958@gmail.com>
2024-11-18 02:34:39 +0600

    vehicle sub features are added, awaiting post on stores

--- lib/core/base_component/base/base_widgets/app_bar.dart
+++ lib/core/base_component/base/base_widgets/app_bar.dart
@@ -20,79 +20,39 @@ class NovaWheelsAppBar extends StatelessWidget implements PreferredSizeWidget {
   });
 
   @override
-  Size get preferredSize => AppBar().preferredSize;
+  Size get preferredSize => Size.fromHeight(AppBar().preferredSize.height + 1);
 
   @override
   Widget build(BuildContext context) {
-    final Color selectedBlue = Color(0xff007AFF);
+    // final Color selectedBlue = Color(0xff007AFF);
     ThemeData theme = Theme.of(context);
 
-    return AppBar(
-      leadingWidth: context.width * 0.25,
-      elevation: 0,
-      scrolledUnderElevation: 0,
-      leading: isBackButtonEnabled ? const UrgentBackButton() : null,
-      backgroundColor: bgColor ?? theme.appBarTheme.backgroundColor,
-      automaticallyImplyLeading: isBackButtonEnabled,
-      actions: actions,
-      iconTheme: theme.appBarTheme.iconTheme,
-      centerTitle: true,
-      title: Text(
-        title,
-        style: context.theme.textTheme.titleMedium?.copyWith(
-          // fontSize: context.width * 0.041,
-          fontWeight: FontWeight.w600,
+    return Column(
+      children: [
+        AppBar(
+          leadingWidth: context.width * 0.25,
+          elevation: 0,
+          scrolledUnderElevation: 0,
+          leading: isBackButtonEnabled ? const UrgentBackButton() : null,
+          backgroundColor: bgColor ?? theme.appBarTheme.backgroundColor,
+          automaticallyImplyLeading: isBackButtonEnabled,
+          actions: actions,
+          iconTheme: theme.appBarTheme.iconTheme,
+          centerTitle: true,
+          title: Text(
+            title,
+            style: context.theme.textTheme.titleMedium?.copyWith(
+              // fontSize: context.width * 0.041,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
         ),
-      ),
+        Divider(
+          height: 1,
+          thickness: 1,
+          color: Colors.grey[300],
+        ),
+      ],
     );
   }
 }
-
-//import 'package:common_services/presentation/widget_gallery/app_bar/urgent_back_button.dart';
-// import 'package:flutter/material.dart';
-// import 'package:utilities/extensions/context_extension.dart';
-//
-// class UrgentAppBar extends StatelessWidget implements PreferredSizeWidget {
-//   final String title;
-//   final List<Widget>? actions;
-//   final bool isBackButtonEnabled;
-//   final Color? bgColor;
-//   final Color? tittleTextColor;
-//
-//   const UrgentAppBar({
-//     super.key,
-//     required this.title,
-//     this.bgColor,
-//     this.actions,
-//     this.isBackButtonEnabled = true,
-//     this.tittleTextColor,
-//   });
-//
-//   @override
-//   Size get preferredSize => AppBar().preferredSize;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final Color selectedBlue = Color(0xff007AFF);
-//     ThemeData theme = Theme.of(context);
-//
-//     return AppBar(
-//       leadingWidth: context.width * 0.25,
-//       elevation: 0,
-//       scrolledUnderElevation: 0,
-//       leading: isBackButtonEnabled ? const UrgentBackButton() : null,
-//       backgroundColor: bgColor ?? theme.appBarTheme.backgroundColor,
-//       automaticallyImplyLeading: isBackButtonEnabled,
-//       actions: actions,
-//       iconTheme: theme.appBarTheme.iconTheme,
-//       centerTitle: true,
-//       title: Text(
-//         title,
-//         style: context.theme.textTheme.titleMedium?.copyWith(
-//           // fontSize: context.width * 0.041,
-//           fontWeight: FontWeight.w600,
-//         ),
-//       ),
-//     );
-//   }
-// }
